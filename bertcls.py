96f7859b1020f2a1444b2dd654452c42e8ff82fa
nuged <ghosnesh@gmail.com>
2020-12-21 00:51:23 +0300

    remove similar

--- bertcls.py
+++ bertcls.py
@@ -46,17 +46,17 @@ class Classifier(nn.Module):
         self.config.max_position_embeddings = 256
         # self.config.hidden_dropout_prob = 0.4
         # self.config.attention_probs_dropout_prob = 0.4
-        self.fc = nn.Linear(self.config.hidden_size, 32)
-        self.fc2 = nn.Linear(32, 2)
-        self.drop = nn.Dropout(0.2)
+        self.fc = nn.Linear(self.config.hidden_size, 2)
+        # self.fc2 = nn.Linear(32, 2)
+        # self.drop = nn.Dropout(0.2)
 
     def forward(self, *args, **kwargs):
-        x = self.bert(*args, **kwargs).last_hidden_state[:, 0, :]
+        x = self.bert(*args, **kwargs).last_hidden_state.mean(axis=1)
         x = nn.LeakyReLU()(x)
         x = self.fc(x)
-        x = nn.LeakyReLU()(x)
-        x = self.drop(x)
-        x = self.fc2(x)
+        # x = nn.LeakyReLU()(x)
+        # x = self.drop(x)
+        # x = self.fc2(x)
         return x
 
 
@@ -296,4 +296,7 @@ if __name__ == "__main__":
     # data = p.map(str.strip, data)
     # p.close()
 
-    CV(data, labels, nfolds=3, train_epochs=3, lr=1e-4, bs=64, wd=1e-3)
+    cls = Classifier().to(device)
+
+    simple_test(cls, AdamW, data, labels, 10, lr=1e-4, bs=64, wd=1e-3, title='yeboii')
+    # CV(data, labels, nfolds=3, train_epochs=3, lr=1e-4, bs=64, wd=1e-3)
