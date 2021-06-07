280c6fc308734ff6119214dd95bcf0d1ca5c489e
nuged <ghosnesh@gmail.com>
2021-06-07 17:56:22 +0300

    preparing

--- bertcls.py
+++ bertcls.py
@@ -1,25 +1,22 @@
-import random
-
-from transformers import BertTokenizer, BertForSequenceClassification, BertModel, AdamW
+from transformers import BertTokenizer, BertModel, AdamW
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from data_preparation import *
-from multiprocessing import Pool
 import torch
 from sklearn.model_selection import StratifiedKFold, train_test_split
-import torch.optim as optim
+from torch.optim.lr_scheduler import OneCycleLR
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import torch.nn.functional as F
-from collections import defaultdict
+from sklearn.metrics import accuracy_score, f1_score
 from IPython import display
+import pickle
+from math import ceil
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.set_num_threads(10)
 
 
-class myDataset(Dataset):
+class myDataset(Dataset):  # here
     def __init__(self, ids, texts, labels):
         super(myDataset, self).__init__()
         self.X = [texts[i] for i in ids]
@@ -32,11 +29,11 @@ class myDataset(Dataset):
         return len(self.X)
 
 
-class Classifier(nn.Module):
-    def __init__(self):
-        super(Classifier, self).__init__()
+class Klassifier(nn.Module):
+    def __init__(self, n_outputs=2):
+        super(Klassifier, self).__init__()
         self.bert = BertModel.from_pretrained("DeepPavlov/rubert-base-cased-sentence")
-
+        self.nout = n_outputs
         # for p in self.bert.parameters():
         #     p.requires_grad = False
 
@@ -50,99 +47,94 @@ class Classifier(nn.Module):
         # self.config.attention_probs_dropout_prob = 0.4
         self.fc = nn.Linear(self.config.hidden_size, 1024)
         self.fc2 = nn.Linear(1024, 2)
-        # self.drop = nn.Dropout(0.1)
+        self.drop = nn.Dropout(0.5)
+        self.tokenizer = BertTokenizer.from_pretrained('DeepPavlov/rubert-base-cased-sentence')
+        self.tokenizer.model_max_length = 10
 
     def forward(self, *args, **kwargs):
         x = self.bert(*args, **kwargs).last_hidden_state.mean(axis=1)
-        x = nn.LeakyReLU(0.1)(x)
-        # x = self.drop(x)
+        x = nn.ReLU()(x)
+        x = self.drop(x)
+
         x = self.fc(x)
-        x = nn.LeakyReLU(0.1)(x)
+        x = nn.ReLU()(x)
         # x = self.drop(x)
         x = self.fc2(x)
         return x
 
-
-tokenizer = BertTokenizer.from_pretrained("DeepPavlov/rubert-base-cased-sentence", do_lower_case=False)
-tokenizer.model_max_length = 128
-
-def todevice(d):
+def todevice(d):  # here
     for k in d:
         # print(k, d[k].size())
-        d[k] = d[k][:, :512].to(device)
+        d[k] = d[k].to(device)
 
 
 def apply_model(model, texts):
-    input_data = tokenizer(texts, padding=True, return_tensors='pt', max_length=128, truncation=True)
+    tokenizer = model.tokenizer
+    if type(texts[0]) is str:
+        input_data = tokenizer(texts, padding=True, return_tensors='pt', truncation=True)
+    else:
+        input_data = tokenizer(*texts, padding=True, return_tensors='pt', truncation=True)
     todevice(input_data)
     output = model(**input_data)
     return output
 
-def get_scores(y_true, y_pred):
-    result = {'accuracy': accuracy_score(y_true, y_pred) * 100, 'precision': precision_score(y_true, y_pred) * 100,
-              'recall': recall_score(y_true, y_pred) * 100, 'F1': f1_score(y_true, y_pred) * 100}
-    return result
 
-
-def train_epoch(model, train_loader, criterion, optimizer, report_every=0, scheduler=None):
+def train_epoch(model, optimizer, scheduler, train_dl):
+    criterion = nn.CrossEntropyLoss()
     model.train()
     loss_history = []
     running_loss = 0
-    for i, (texts, labels) in enumerate(train_loader):
+    for i, (texts, labels) in enumerate(train_dl):
         optimizer.zero_grad()
         output = apply_model(model, texts)
         loss = criterion(output, labels.to(device).view(-1))
         loss.backward()
         optimizer.step()
-        if (scheduler):
+        if scheduler is not None:
             scheduler.step()
         loss_history.append(loss.item())
         running_loss += loss.item()
-        if report_every and i % report_every == report_every - 1:
-            print(f"iter {i}, loss={running_loss / report_every:3.2f}")
-            running_loss = 0
-
     return loss_history
 
 
-def train(nepochs, model, train_loader, criterion, optimizer, test_loader=None, report_every=0, scheduler=None):
-    print('started training')
-    for epoch in range(nepochs):
-        print(f'epoch #{epoch}')
-        epoch_history = train_epoch(model, train_loader, criterion, optimizer, report_every, scheduler)
-        if test_loader is not None:
-            test_loss, y_true, y_pred, probs = eval(model, test_loader, criterion)
-            scores = get_scores(y_true, y_pred)
-            yield epoch_history, test_loss, scores, probs
+def train(model, optimizer, scheduler, train_dl, epochs, test_dl=None):
+    model.train()
+    avg = 'binary' if model.nout <= 2 else 'macro'
+    for epoch in range(epochs):
+        epoch_history = train_epoch(model, optimizer, scheduler, train_dl)
+        if test_dl is not None:
+            test_loss, y_true, probs = predict(model, test_dl)
+            y_pred = np.argmax(probs, axis=1)
+            scores = {'F1': f1_score(y_true, y_pred, average=avg) * 100,
+                      'acc': accuracy_score(y_true, y_pred) * 100}
+            yield epoch_history, test_loss, scores, y_true, probs
         else:
             yield epoch_history
 
 
-def eval(model, dl, criterion):
+def predict(model, dl):
+    criterion = nn.CrossEntropyLoss()
     model.eval()
-    mean_loss = 0
-    count = 0
-    y_pred = []
+
     y_true = []
     probs = []
-    for texts, labels in dl:
-        with torch.no_grad():
+    mean_loss = 0
+    count = 0
+
+    with torch.no_grad():
+        for texts, labels in dl:
             output = apply_model(model, texts)
-            probs.extend(nn.Softmax(dim=1)(output)[:, 1].detach().tolist())
+            probs.extend(nn.Softmax(dim=1)(output).detach().tolist())
+
             loss = criterion(output, labels.to(device).view(-1))
             mean_loss += loss.item()
             count += 1
-            pred = output.argmax(axis=1).detach().tolist()
-            y_pred.extend(pred)
+
             y_true.extend((labels.tolist()))
-    return mean_loss / count, y_true, y_pred, probs
+    return mean_loss / count, y_true, probs
 
 
-def moving_average(x, w):
-    return np.convolve(x, np.ones(w), 'valid') / w
-
-
-def plot_history(loss_history, validation, F1, acc, sizes, title="", clear_output=True):
+def plot_history(loss_history, validation, F1, acc, sizes, title="", clear_output=True): # here
     if clear_output:
         display.clear_output(wait=True)
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(17, 7))
@@ -168,132 +160,113 @@ def plot_history(loss_history, validation, F1, acc, sizes, title="", clear_outpu
     return fig
 
 
-metrics = ['accuracy', 'precision', 'recall', 'F1']
+def cross_validation(model, data, labels, n_splits=4, theme=''):
+    BS = 32
+    EPOCHS = 7
 
+    cv = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=3)
+    predictions = []
+    gts = []
 
-def CV(data, labels, nfolds=4, train_epochs=3, lr=1e-6, bs=32, wd=1e-6):
-    # TODO: добавить отрисовку и сохранение графика усредненных по всем фолдам потерь на тесте и обучении
-    kf = StratifiedKFold(n_splits=nfolds, shuffle=True, random_state=7)
-    criterion = nn.CrossEntropyLoss()
+    torch.manual_seed(7)
 
-    epoch_size = len(data) / nfolds * (nfolds - 1)
-    epoch_size = int(epoch_size)
-    epoch_size = (epoch_size + bs - 1) // bs - 9
+    optimizer = AdamW(model.parameters(), lr=1e-5, weight_decay=1e-6)
 
-    scores_avg = {m: np.zeros(train_epochs) for m in metrics}  # metric -> [m_at_ep_1, m_at_ep_2, ... ]
-    loss_history = np.zeros((train_epochs, epoch_size))
-    val_history = np.zeros(train_epochs)
+    torch.save({
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+    }, f'models/{type(model).__name__}_cv.pt')
 
-    for fold, (train_ids, test_ids) in enumerate(kf.split(data, labels)):
-        print(f"Fold #{fold}")
-        torch.manual_seed(7)
-        cls = Classifier().to(device)
-        optimizer = AdamW(cls.parameters(), lr=lr, weight_decay=wd)
-
-        train_ds = myDataset(train_ids, data, labels)
-        test_ds = myDataset(test_ids, data, labels)
-        train_dl = DataLoader(train_ds, batch_size=bs, shuffle=True)
-        test_dl = DataLoader(test_ds, batch_size=bs)
-
-        epoch = 0
-
-        for epoch_history, test_loss, test_scores in train(train_epochs, cls, train_dl,
-                                                           criterion, optimizer, test_dl):
-
-            epoch_history = np.array(moving_average(epoch_history, 10))[:epoch_size]
-            loss_history[epoch] += epoch_history
-            val_history[epoch] += test_loss
-
-            for m, val in test_scores.items():
-                scores_avg[m][epoch] += val
-            epoch += 1
+    for n, (train_idx, test_idx) in enumerate(cv.split(data, labels)):
+        print(f'{n}-th fold')
 
         if device == 'cuda':
             torch.cuda.empty_cache()
 
-    for m in scores_avg:
-        scores_avg[m] /= nfolds
-    loss_history /= nfolds
-    val_history /= nfolds
-    sizes = np.cumsum([epoch_size for i in range(train_epochs)])
+        train_ds = myDataset(train_idx, data, labels)
+        test_ds = myDataset(test_idx, data, labels)
+        train_dl = DataLoader(train_ds, batch_size=BS, shuffle=True)
+        test_dl = DataLoader(test_ds, batch_size=BS, shuffle=False)
 
-    fig = plot_history(loss_history.reshape(-1), val_history, scores_avg['F1'], scores_avg['accuracy'],
-                       sizes, title=f"CV: lr={lr}, bs={bs}, wd={wd}", clear_output=False)
+        checkpoint = torch.load(f'models/{type(model).__name__}_cv.pt')
 
-    for epoch in range(train_epochs):
-        print(f"epoch #{epoch}")
-        print(f"\tval_loss={val_history[epoch]:4.3f}")
-        for m, val in scores_avg.items():
-            print(f"\t{m}={val[epoch]:4.2f}")
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        scheduler = lr_scheduler.OneCycleLR(optimizer, max_lr=5e-5, steps_per_epoch=ceil(len(train_ds) / BS),
+                                            epochs=EPOCHS, anneal_strategy='linear')
+
+        for _ in train(model, optimizer, scheduler, train_dl, EPOCHS, None):
+            pass
+
+        loss, y_test, y_pred = predict(model, test_dl)
+        predictions.extend(y_pred)
+        gts.extend(y_test)
+
+        del train_ds
+        del train_dl
+        del test_ds
+        del test_dl
+        gc.collect()
+
+        if device == 'cuda':
+            torch.cuda.empty_cache()
+
+    conf = confusion_matrix(gts, predictions)
+
+    with open(f'{TASK}_{theme}_{type(model).__name__}_CV.pk', 'wb') as f:
+        pickle.dump(conf, f)
 
 
-def simple_test(cls, optim_cls, data, labels, train_epochs=3, lr=1e-6, bs=32, wd=1e-6, title=""):
-    ds = myDataset(range(len(data)), data, labels)
-    train_ds, test_ds = train_test_split(ds, test_size=0.2, shuffle=True, random_state=7,
-                                                            stratify=labels)
-    train_dl = DataLoader(train_ds, batch_size=bs, shuffle=True)
-    test_dl = DataLoader(test_ds, batch_size=bs)
-
+def simple_test(model, params, train_ds, test_ds, title=''):
     torch.manual_seed(7)
-    optimizer = optim_cls(cls.parameters(), lr=lr, weight_decay=wd)
-    criterion = nn.CrossEntropyLoss()
+    name = type(model).__name__
+    BS = params['BS']
+    EPOCHS = params['n_epochs']
+    optimizer = AdamW(model.parameters(), lr=params['lr'], weight_decay=params['w_decay'])
+    scheduler = None if not params['shed'] \
+        else OneCycleLR(optimizer, max_lr=5e-5, steps_per_epoch=ceil(len(train_ds) / BS),
+                        epochs=EPOCHS, anneal_strategy='linear')
 
-    epoch = 0
+    train_dl = DataLoader(train_ds, batch_size=BS, shuffle=True)
+    test_dl = DataLoader(test_ds, batch_size=BS, shuffle=False)
+
     loss_history = []
     validation = []
     sizes = []
-
-    best_scores = {m: 0 for m in ['accuracy', 'precision', 'recall', 'F1', 'val_loss']}
-    best_epoch = best_scores.copy()
-    best_scores['val_loss'] = 1000
-
-    log = []
-
     F1 = []
     acc = []
 
-    for epoch_history, test_loss, test_scores in train(train_epochs, cls, train_dl,
-                                                       criterion, optimizer, test_dl):
-        epoch_history = moving_average(epoch_history, 10).tolist()
-        log.append(test_scores)
+    y_history = []
+    epoch = 0
+    best_f1 = 0
+    for epoch_history, test_loss, test_scores, y_true, probs in train(model, optimizer, scheduler, train_dl, EPOCHS,
+                                                                       test_dl):
+        f1 = test_scores['F1']
+        if f1 > best_f1:
+            print(f'epoch #{epoch}:\treached better score ({f1:4.2f} vs {best_f1:4.2f})')
+            best_f1 = f1
+            torch.save({
+                'epoch': epoch,
+                'model': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'scheduler': scheduler.state_dict(),
+            }, f'models/{title}_{name}.pt')
+
         loss_history.extend(epoch_history)
         validation.append(test_loss)
         sizes.append(len(loss_history))
         F1.append(test_scores['F1'])
-        acc.append(test_scores['accuracy'])
+        acc.append(test_scores['acc'])
+        y_history.append((y_true, probs))
 
-        fig = plot_history(loss_history, validation, F1, acc, sizes, title)
-
-        for m, val in test_scores.items():
-            if val > best_scores[m]:
-                best_scores[m] = val
-                best_epoch[m] = epoch
-        if test_loss < best_scores['val_loss']:
-            best_scores['val_loss'] = test_loss
-            best_epoch['val_loss'] = epoch
+        fig = plot_history(loss_history, validation, F1, acc, sizes, title='')
 
         epoch += 1
 
-    save_results(fig, log, best_scores, best_epoch, title)
-
-
-def save_results(fig, log, best_scores, best_epoch, title=""):
-    fig.savefig(f'plots/{title}.png')
-    with open(f"logs/{title}.log", 'w') as f:
-        for m, val in best_scores.items():
-            print(f"{m} = {val:4.2f}, epoch = {best_epoch[m]}", file=f)
-        best = log[best_epoch['val_loss']]
-        print(f"scores at epoch={best_epoch['val_loss']}:", file=f)
-        for m, val in best.items():
-            print(f"{m} = {val:4.2f}", file=f)
-
-# TODO:
-# попробовать:
-# увеличить дропаут
-# вовзращение метрик после каждой эпохи обучения и их усреднение по фолдам, возможно,
-# стоит возвращать список с метриками после каждой эпохи
-# выключить слои?
-# регуляризация
+    fig.savefig(f'plots/{title}_{name}_plot.png')
+    idx = np.argmax(F1)
+    with open(f'history/{title}_{name}_probs.pk', 'wb') as f:
+        pickle.dump(y_history[idx], f)
 
 
 if __name__ == "__main__":
@@ -301,26 +274,23 @@ if __name__ == "__main__":
     labels = [1] * len(data)
     data.extend(read_data('mydata/opinion mining/neg_final.txt'))
     labels += [0] * (len(data) - len(labels))
-    ds = myDataset(random.sample(range(len(data)), 1000), data, labels)
-    exit(0)
-    print('loaded', len(data))
-    # p = Pool(processes=4)
-    # data = p.map(remove_links, data)
-    # data = p.map(remove_emoji, data)
-    # data = p.map(fix_start, data)
-    # data = p.map(fix_quotes, data)
-    # data = p.map(remove_sources, data)
-    # data = p.map(str.strip, data)
-    # p.close()
 
-    cls = Classifier().to(device)
-
-    # simple_test(cls, AdamW, data, labels, 7, lr=1e-5, bs=64, wd=0, title='yeboii')
-    torch.manual_seed(5)
     ds = myDataset(range(len(data)), data, labels)
-    train_dl = DataLoader(ds, batch_size=64, shuffle=True)
-    loss = nn.CrossEntropyLoss()
-    opt = AdamW(cls.parameters(), lr=1e-5, weight_decay=0)
-    for _ in train(7, cls, train_dl, loss, opt, report_every=10):
-        pass
-    torch.save(cls.state_dict(), 'models/classifier.pt')
+    print(f'loaded {len(ds)} examples')
+    train_ds, test_ds = train_test_split(ds, test_size=0.3, shuffle=True, random_state=35,
+                                         stratify=labels)
+
+    cls = Klassifier().to(device)
+
+    print(cls.tokenizer)
+
+    params = {
+        'n_epochs': 10,
+        'BS': 64,
+        'lr': 1e-5,
+        'max_lr': 1e-4,
+        'w_decay': 1e-5,
+        'shed': True
+    }
+
+    simple_test(cls, params, train_ds, test_ds, title='sentiment_qa')
